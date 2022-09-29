982b75b10b9d59056314ea7f39d48941273bff0c
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-29 19:50:42 -0300

    Requisito 14 - feito

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,74 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const getAllNames = (e) => e.name;
+
+const theFilter = (gender, residents) => {
+  const writeNames = residents.filter((e) => e.sex === gender).map(getAllNames);
+  return writeNames;
+};
+
+const getOptions = (gender, sorted) => species.reduce((acc, { location, name, residents }) => {
+  const newAcc = acc;
+  if (sorted === undefined) {
+    newAcc[location].push({ [name]: theFilter(gender, residents) });
+  } else {
+    newAcc[location].push({ [name]: theFilter(gender, residents).sort() });
+  }
+  return newAcc;
+}, {
+  NE: [],
+  NW: [],
+  SE: [],
+  SW: [],
+});
+
+const findArea = () => species.reduce((acc, { location, name }) => {
+  const newAcc = acc;
+  newAcc[location].push(name);
+  return newAcc;
+}, {
+  NE: [],
+  NW: [],
+  SE: [],
+  SW: [],
+});
+
+const findAnimalName = (sorted) => species.reduce((acc, { location, name, residents }) => {
+  const newAcc = acc;
+  if (sorted === undefined) {
+    newAcc[location].push({ [name]: residents.map((nomes) => nomes.name) });
+  } else {
+    newAcc[location].push({ [name]: residents.map((nomes) => nomes.name).sort() });
+  }
+  return newAcc;
+}, {
+  NE: [],
+  NW: [],
+  SE: [],
+  SW: [],
+});
+
+const verifyPt1 = (includeNames, sorted, sex) => {
+  if (!includeNames) return findArea();
+  if (includeNames && !sorted && !sex) return findAnimalName(sorted);
+  return false;
+};
+
+const verifyPt2 = (options, sorted, sex) => {
+  if (options.includeNames && sorted !== undefined && !sex) return true;
+  return false;
+};
+
 function getAnimalMap(options) {
-  // nada ainda, dificilll...
+  if (options === undefined) return findArea();
+  const { includeNames, sorted, sex } = options;
+  if (verifyPt1(includeNames, sorted, sex)) return verifyPt1(includeNames, sorted, sex);
+  if (verifyPt2(options, sorted, sex)) {
+    return findAnimalName(sorted);
+  }
+  if (sex) return getOptions(sex, sorted);
 }
 
 module.exports = getAnimalMap;
