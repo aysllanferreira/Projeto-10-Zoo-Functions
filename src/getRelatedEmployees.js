405a2e8c9340cb8c3c8a64852c469579d2d90041
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-29 16:25:32 -0300

    Requisito 04 - feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -7,7 +7,11 @@ function isManager(id) {
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees.filter(({ managers }) => managers.includes(managerId))
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
