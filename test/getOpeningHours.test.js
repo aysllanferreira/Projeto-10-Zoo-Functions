85d3a4b08b095c6a8849ad651114f7d038f253ca
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-29 15:16:40 -0300

    Testes: verifico quando zoo esta aberto

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -5,7 +5,11 @@ describe('Testes da função getOpeningHours', () => {
     expect(typeof getOpeningHours).toBe('function');
   });
 
-  it('Verifico se qualquer horario de segunda o zoo ta fechado', () => {
+  it('Verifica se qualquer horario de segunda o zoo ta fechado', () => {
     expect(getOpeningHours('Monday', '12:00-AM')).toBe('The zoo is closed');
+  });
+
+  it('Verifica se o zoo esta aberto num dia que deveria estar.', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
   });
 });
