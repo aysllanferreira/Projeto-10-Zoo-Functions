87ba25a8f29fc79cef6279c31a0d28694763e9bd
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-29 15:10:37 -0300

    Cobertura handlerElephantes 100%

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,7 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it('Verifica quantos elefantes residem no zoo', () => {
+  it('Verifica quantos elefantes residem no zoo!', () => {
     expect(handlerElephants('count')).toBe(4);
   });
 
