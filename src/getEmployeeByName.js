bd151d54a7641ef9bc7475d1a79ba4fe343d428e
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-29 16:07:09 -0300

    Requisito 03 - feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return employees.find((pessoa) => employeeName === pessoa
+    .firstName || employeeName === pessoa.lastName);
 }
 
 module.exports = getEmployeeByName;
