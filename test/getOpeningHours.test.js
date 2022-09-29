44107c6a88f67cf19c25230eca4a662d2e4d2a31
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-29 15:14:44 -0300

    Testes: Verifico se eh uma funcao

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,7 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Verifica se getOpeningHours eh uma funcao', () => {
+    expect(typeof getOpeningHours).toBe('function');
+  });
+});
