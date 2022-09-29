e51bb3beb8c3d274ae30c6ca0c6ebb5b6c397d91
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-29 14:56:21 -0300

    Testes: Verifiquei quantos elefantes vivem no zoo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,6 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
   // seu código aqui
-  // oi
 }
 
 module.exports = getSpeciesByIds;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica quantos elefantes residem no zoo', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+});
