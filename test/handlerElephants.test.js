ed15ba04e48e9fc9366618643a7af42bddbcd1dc
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-29 14:59:09 -0300

    Testes: Verifiquei se existe um nome especifico.

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -4,4 +4,8 @@ describe('Testes da função HandlerElephants', () => {
   it('Verifica quantos elefantes residem no zoo', () => {
     expect(handlerElephants('count')).toBe(4);
   });
+
+  it('Verifica se existe um elefanto com um nome especifico.', () => {
+    expect(handlerElephants('names')).toContain('Bea');
+  });
 });
