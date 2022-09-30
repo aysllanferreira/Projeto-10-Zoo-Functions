38430c410e985e7eea567580a8cb5c516490ba6e
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-30 19:39:42 -0300

    Requisito 05 - refatorado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,21 +3,13 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 function countAnimals(animal) {
-  const getSpecies = species.reduce((obj1, obj2) => {
-    const getResults = obj1;
-    getResults[obj2.name] = obj2.residents.length;
-    return obj1;
-  }, {});
-  if (!animal) {
-    return getSpecies;
-  }
+  const getSpecies = species.map(({ name, residents }) => ({ [name]: residents.length })).sort();
+  if (!animal) return Object.assign({ }, ...getSpecies);
+
   const quantity = species.find((animals) => animals.name === animal.specie);
   const gender = quantity.residents.filter((animals) => animals.sex === animal.sex);
-  if (!animal.sex) {
-    return quantity.residents.length;
-  } if (animal.sex) {
-    return gender.length;
-  }
+  if (!animal.sex) return quantity.residents.length;
+  if (animal.sex) return gender.length;
 }
 
 module.exports = countAnimals;
