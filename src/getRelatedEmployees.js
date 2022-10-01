31960f5c79c2a66645e86476143bdb8ce345a5df
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-30 22:29:57 -0300

    Req 4 refatorado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,9 +2,7 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
-function isManager(id) {
-  return employees.some(({ managers }) => managers.includes(id));
-}
+const isManager = (id) => employees.some(({ managers }) => managers.includes(id));
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId)) {
