f3307904d78939b9dd553e9f8970d07c3af9b93e
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-03 07:06:59 -0300

    req 14 refatorado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -9,50 +9,32 @@ const theFilter = (gender, residents) => {
   return writeNames;
 };
 
-const getOptions = (gender, sorted) => species.reduce((acc, { location, name, residents }) => {
-  const newAcc = acc;
-  if (sorted === undefined) {
-    newAcc[location].push({ [name]: theFilter(gender, residents) });
-  } else {
-    newAcc[location].push({ [name]: theFilter(gender, residents).sort() });
-  }
-  return newAcc;
-}, {
-  NE: [],
-  NW: [],
-  SE: [],
-  SW: [],
-});
+const optNormal = (name, gender, residents) => ({ [name]: theFilter(gender, residents) });
+const optSorted = (name, gender, residents) => ({ [name]: theFilter(gender, residents).sort() });
+const locNormal = (name, gender, residents) => ({ [name]: residents.map((nomes) => nomes.name) });
+const locSorted = (name, gender, residents) => ({ [name]: residents
+  .map((nomes) => nomes.name).sort() });
+
+const getOptions = (gender, sorted, callback, callback2) => species
+  .reduce((acc, { location, name, residents }) => {
+    const newAcc = acc;
+    if (sorted === undefined) {
+      newAcc[location].push(callback(name, gender, residents));
+    } else {
+      newAcc[location].push(callback2(name, gender, residents));
+    }
+    return newAcc;
+  }, { NE: [], NW: [], SE: [], SW: [] });
 
 const findArea = () => species.reduce((acc, { location, name }) => {
   const newAcc = acc;
   newAcc[location].push(name);
   return newAcc;
-}, {
-  NE: [],
-  NW: [],
-  SE: [],
-  SW: [],
-});
-
-const findAnimalName = (sorted) => species.reduce((acc, { location, name, residents }) => {
-  const newAcc = acc;
-  if (sorted === undefined) {
-    newAcc[location].push({ [name]: residents.map((nomes) => nomes.name) });
-  } else {
-    newAcc[location].push({ [name]: residents.map((nomes) => nomes.name).sort() });
-  }
-  return newAcc;
-}, {
-  NE: [],
-  NW: [],
-  SE: [],
-  SW: [],
-});
+}, { NE: [], NW: [], SE: [], SW: [] });
 
 const verifyPt1 = (includeNames, sorted, sex) => {
   if (!includeNames) return findArea();
-  if (includeNames && !sorted && !sex) return findAnimalName(sorted);
+  if (includeNames && !sorted && !sex) return getOptions(sex, sorted, locNormal, locSorted);
   return false;
 };
 
@@ -66,9 +48,9 @@ function getAnimalMap(options) {
   const { includeNames, sorted, sex } = options;
   if (verifyPt1(includeNames, sorted, sex)) return verifyPt1(includeNames, sorted, sex);
   if (verifyPt2(options, sorted, sex)) {
-    return findAnimalName(sorted);
+    return getOptions(sex, sorted, locNormal, locSorted);
   }
-  if (sex) return getOptions(sex, sorted);
+  if (sex) return getOptions(sex, sorted, optNormal, optSorted);
 }
 
 module.exports = getAnimalMap;
